3aa25d54d2e57f4b646490eb81fd0cac764802d8
stephenwang0317 <stephenwang0317@gmail.com>
2024-03-12 19:30:39 +0800

    token bucket

---
+++ src/test/java/com/wjm/bootbook/component/TokenBucketTest.java
@@ -0,0 +1,36 @@
+package com.wjm.bootbook.component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.Duration;
+
+@SpringBootTest
+@EnableScheduling
+public class TokenBucketTest {
+
+    @Autowired
+    RedisTokenBucket bucket;
+
+    @Test
+    void test() throws InterruptedException {
+        for (int i = 0; i < 50; i++) {
+            Thread.sleep(100);
+            System.out.println(i + " " + bucket.getToken() + " ");
+        }
+    }
+
+
+    @Test
+    void use() throws InterruptedException {
+        for (int i = 0; i < 20; i++) {
+            boolean token = bucket.getToken();
+            System.out.println("token " + token + " " + i);
+            Thread.sleep(Duration.ofMillis(500));
+        }
+    }
+
+}
